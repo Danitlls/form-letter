2f669e41ba2637aadcf9d507373b60eacbbacbb7
Patrick Wells <patmicwells@gmail.com>
2017-04-26 20:49:04 -0700

    More fun stuffgit status

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,28 @@
 $(document).ready(function() {
+	var favoriteColor;
+	var name;
+
   $("#formletter").submit(function(event) {
-      var name = $("input#name").val();
+      name = $("input#name").val();
       $(".name").text(name);
       $("#letter").show();
       event.preventDefault();
   });
-});
-// Test for getting hex value in console
 
-// var favoriteColor;
-//
-// $(document).ready(function() {
-//   $("#colorwheel").submit(function(event){
-//     favoriteColor = $("#color").val();
-//     event.preventDefault();
-//     console.log(favoriteColor);
-//   });
-//  });
+  // Test for getting hex value in console
+
+  $("#colorwheel").submit(function(event){
+    favoriteColor = $("#color").val();
+    event.preventDefault();
+    console.log(favoriteColor);
+    var array = favoriteColor.split('')
+    console.log(array)
+    array[1] = 'f'
+    console.log(array)
+    var background = array.join('')
+    console.log(background)
+    var element = $('#nameWithColor')
+    element.append('<div id="node" style="background-color: ' + background +';"><h3 style="color: ' + favoriteColor +';">' + (name || 'Patrick') + '</h3></div>')
+
+  });
+});
